ecb681b66fb757b3b87c410754a3580cae4d499a
Zach <104291766+zachary-o@users.noreply.github.com>
2023-05-21 19:35:22 +0300

    upload the game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
+
+import VersusComputer from "./components/VersusComputer";
+import VersusHuman from "./components/VersusHuman";
 
 function App() {
+  const [steps, setSteps] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [player, setPlayer] = useState("X");
+  const [mode, setMode] = useState(null);
+  const [toggle, setToggle] = useState(true);
+
+  const handleSetVsHumanMode = () => {
+    setMode(true);
+    setToggle(false);
+  };
+
+  const handleSetVsAIMode = () => {
+    setMode(false);
+    setToggle(false);
+  };
+
+  const renderGame = () => {
+    if (mode) {
+      return (
+        <VersusHuman
+          steps={steps}
+          setSteps={setSteps}
+          player={player}
+          setPlayer={setPlayer}
+          setToggle={setToggle}
+          setMode={setMode}
+        />
+      );
+    } else if (mode === false) {
+      return (
+        <VersusComputer
+          steps={steps}
+          setSteps={setSteps}
+          player={player}
+          setPlayer={setPlayer}
+          setToggle={setToggle}
+          setMode={setMode}
+        />
+      );
+    }
+  };
+
+  console.log(mode, toggle);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {toggle ? (
+        <div className="toggle-container">
+          <h1>
+            Do you want to play with your friend or the super-enhanced AI Model
+            v69?
+          </h1>
+          <div className="buttons-container">
+            <button onClick={handleSetVsHumanMode}>
+              Human VS Human
+            </button>
+            <button onClick={handleSetVsAIMode} >
+              Human VS AI
+            </button>
+          </div>
+        </div>
+      ) : null}
+      {renderGame()}
     </div>
   );
 }
---
+++ src/components/VersusHuman.jsx
@@ -0,0 +1,105 @@
+const VersusHuman = ({
+  steps,
+  setSteps,
+  player,
+  setPlayer,
+  setToggle,
+  setMode,
+}) => {
+  let winner = null;
+
+  const handleSquareClick = (index) => {
+    if (winner) {
+      return;
+    }
+    if (steps[index] === null) {
+      const updatedSteps = [...steps];
+      updatedSteps[index] = player;
+      setSteps(updatedSteps);
+
+      setPlayer(player === "X" ? "O" : "X");
+    }
+  };
+
+  const checkWinner = () => {
+    const winCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winCombinations.length; i++) {
+      const [a, b, c] = winCombinations[i];
+
+      if (
+        steps[a] === steps[b] &&
+        steps[a] === steps[c] &&
+        steps[a] &&
+        steps[b] &&
+        steps[c]
+      ) {
+        winner = steps[a];
+      }
+    }
+
+    const filled = steps.every((el) => el);
+    if (filled && !winner) {
+      winner = "Draw";
+    }
+  };
+  checkWinner();
+
+  const titleText = () => {
+    if (!winner) {
+      return `Next player is ${player}`;
+    }
+    if (winner === "Draw") {
+      return "It's a draw";
+    } else {
+      return `The winner is ${winner}`;
+    }
+  };
+
+  const handleNewGame = () => {
+    setSteps(Array(9).fill(null));
+    setPlayer("X");
+  };
+
+  const handleSetToggle = () => {
+    setToggle(true);
+    setMode(null);
+  };
+
+  return (
+    <div className="game-container">
+      <h1>{titleText()}</h1>
+      <div className="board">
+        <div className="board-row">
+          <p onClick={() => handleSquareClick(0)}>{steps[0]}</p>
+          <p onClick={() => handleSquareClick(1)}>{steps[1]}</p>
+          <p onClick={() => handleSquareClick(2)}>{steps[2]}</p>
+        </div>
+        <div className="board-row">
+          <p onClick={() => handleSquareClick(3)}>{steps[3]}</p>
+          <p onClick={() => handleSquareClick(4)}>{steps[4]}</p>
+          <p onClick={() => handleSquareClick(5)}>{steps[5]}</p>
+        </div>
+        <div className="board-row">
+          <p onClick={() => handleSquareClick(6)}>{steps[6]}</p>
+          <p onClick={() => handleSquareClick(7)}>{steps[7]}</p>
+          <p onClick={() => handleSquareClick(8)}>{steps[8]}</p>
+        </div>
+      </div>
+      <div>
+        <button onClick={handleNewGame}>New Game</button>
+        <button onClick={() => handleSetToggle()}>Change Mode</button>
+      </div>
+    </div>
+  );
+};
+export default VersusHuman;
